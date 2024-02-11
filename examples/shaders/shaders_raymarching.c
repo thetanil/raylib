f9c8dcb5b613e64527e9b3d89fe97e851b98e720
Theta Nil <20186755+thetanil@users.noreply.github.com>
2024-02-11 08:58:38 +0100

    build a modded example

--- examples/shaders/shaders_raymarching.c
+++ examples/shaders/shaders_raymarching.c
@@ -1,25 +1,25 @@
 /*******************************************************************************************
-*
-*   raylib [shaders] example - Raymarching shapes generation
-*
-*   NOTE: This example requires raylib OpenGL 3.3 for shaders support and only #version 330
-*         is currently supported. OpenGL ES 2.0 platforms are not supported at the moment.
-*
-*   Example originally created with raylib 2.0, last time updated with raylib 4.2
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2018-2024 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+ *
+ *   raylib [shaders] example - Raymarching shapes generation
+ *
+ *   NOTE: This example requires raylib OpenGL 3.3 for shaders support and only #version 330
+ *         is currently supported. OpenGL ES 2.0 platforms are not supported at the moment.
+ *
+ *   Example originally created with raylib 2.0, last time updated with raylib 4.2
+ *
+ *   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+ *   BSD-like license that allows static linking with closed source software
+ *
+ *   Copyright (c) 2018-2024 Ramon Santamaria (@raysan5)
+ *
+ ********************************************************************************************/
 
 #include "raylib.h"
 
 #if defined(PLATFORM_DESKTOP)
-    #define GLSL_VERSION            330
-#else   // PLATFORM_ANDROID, PLATFORM_WEB -> Not supported at this moment
-    #define GLSL_VERSION            100
+#define GLSL_VERSION 330
+#else // PLATFORM_ANDROID, PLATFORM_WEB -> Not supported at this moment
+#define GLSL_VERSION 100
 #endif
 
 //------------------------------------------------------------------------------------
@@ -33,14 +33,14 @@ int main(void)
     const int screenHeight = 450;
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "raylib [shaders] example - raymarching shapes");
+    InitWindow(screenWidth, screenHeight, "raylib-thetanil [shaders] example - raymarching shapes");
 
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 2.5f, 2.5f, 3.0f };    // Camera position
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.7f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 65.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+    Camera camera = {0};
+    camera.position = (Vector3){2.5f, 2.5f, 3.0f}; // Camera position
+    camera.target = (Vector3){0.0f, 0.0f, 0.7f};   // Camera looking at point
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};       // Camera up vector (rotation towards target)
+    camera.fovy = 65.0f;                           // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;        // Camera projection type
 
     // Load raymarching shader
     // NOTE: Defining 0 (NULL) for vertex shader forces usage of internal default vertex shader
@@ -52,24 +52,24 @@ int main(void)
     int runTimeLoc = GetShaderLocation(shader, "runTime");
     int resolutionLoc = GetShaderLocation(shader, "resolution");
 
-    float resolution[2] = { (float)screenWidth, (float)screenHeight };
+    float resolution[2] = {(float)screenWidth, (float)screenHeight};
     SetShaderValue(shader, resolutionLoc, resolution, SHADER_UNIFORM_VEC2);
 
     float runTime = 0.0f;
 
-    DisableCursor();                    // Limit cursor to relative movement inside the window
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+    DisableCursor();  // Limit cursor to relative movement inside the window
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
         UpdateCamera(&camera, CAMERA_FIRST_PERSON);
 
-        float cameraPos[3] = { camera.position.x, camera.position.y, camera.position.z };
-        float cameraTarget[3] = { camera.target.x, camera.target.y, camera.target.z };
+        float cameraPos[3] = {camera.position.x, camera.position.y, camera.position.z};
+        float cameraTarget[3] = {camera.target.x, camera.target.y, camera.target.z};
 
         float deltaTime = GetFrameTime();
         runTime += deltaTime;
@@ -92,15 +92,15 @@ int main(void)
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
 
-            // We only draw a white full-screen rectangle,
-            // frame is generated in shader using raymarching
-            BeginShaderMode(shader);
-                DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), WHITE);
-            EndShaderMode();
+        // We only draw a white full-screen rectangle,
+        // frame is generated in shader using raymarching
+        BeginShaderMode(shader);
+        DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), WHITE);
+        EndShaderMode();
 
-            DrawText("(c) Raymarching shader by Iñigo Quilez. MIT License.", GetScreenWidth() - 280, GetScreenHeight() - 20, 10, BLACK);
+        DrawText("(c) Raymarching shader by Iñigo Quilez. MIT License.", GetScreenWidth() - 280, GetScreenHeight() - 20, 10, BLACK);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -108,9 +108,9 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    UnloadShader(shader);           // Unload shader
+    UnloadShader(shader); // Unload shader
 
-    CloseWindow();                  // Close window and OpenGL context
+    CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
